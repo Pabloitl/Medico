369d371de140ec37ec6261dbaaac9b7462657305
Pablo <45538982+Pabloitl@users.noreply.github.com>
2020-12-04 23:16:18 +0000

    Some fixes

--- database/Historiales.sql
+++ database/Historiales.sql
@@ -1,6 +1,6 @@
-create database Historiales
+CREATE DATABASE Historiales
 
-use Historiales
+USE Historiales
 
 CREATE TABLE Alumno(
 	No_Control INT NOT NULL,
@@ -9,56 +9,60 @@ CREATE TABLE Alumno(
 	Carrera VARCHAR(30)
 )
 
-alter table Alumno add constraint pk_alumno primary key (No_Control)
+ALTER TABLE Alumno
+ADD CONSTRAINT pk_alumno PRIMARY KEY (No_Control)
 
-create table Medico(
-	Cedula int not null,
-	Nombre varchar(50),
-	Campus int
+CREATE TABLE Medico(
+	Cedula INT NOT NULL,
+	Nombre VARCHAR(50),
+	Campus INT
 )
 
-alter table Medico add constraint pk_medico primary key (Cedula)
+ALTER TABLE Medico
+ADD CONSTRAINT pk_medico PRIMARY KEY (Cedula)
 
-create table Consulta(
-	No_Consulta int not null,
-	No_Control int not null,
-	Cedula int not null,
-	Fecha_consulta date,
-	Diagnostico varchar(30),
-	Tipo_Afeccion varchar(30)
+CREATE TABLE Consulta(
+	No_Consulta INT NOT NULL,
+	No_Control INT NOT NULL,
+	Cedula INT NOT NULL,
+	Fecha_consulta DATE,
+	Diagnostico VARCHAR(30),
+	Tipo_Afeccion VARCHAR(30)
 )
 
-alter table Consulta add constraint pk_consulta primary key (No_Consulta)
+ALTER TABLE Consulta
+ADD CONSTRAINT pk_consulta PRIMARY KEY (No_Consulta)
 
-alter table Consulta ADD constraint fk_alumno_consulta foreign key (No_Control)
-References Alumno (No_Control)
+ALTER TABLE Consulta
+ADD CONSTRAINT fk_alumno_consulta FOREIGN KEY (No_Control) REFERENCES Alumno (No_Control)
 
-alter table Consulta ADD constraint fk_medico_consulta foreign key (Cedula)
-References Medico (Cedula)
+ALTER TABLE Consulta
+ADD CONSTRAINT fk_medico_consulta FOREIGN KEY (Cedula) REFERENCES Medico (Cedula)
 
-create table Medicamento(
-	No_Consulta int not null,
-	Cod_M varchar(8)not null,
-	Nombre varchar(40)
+CREATE TABLE Medicamento(
+	Cod_M VARCHAR(8) NOT NULL,
+	Nombre VARCHAR(40)
 )
 
-alter table Medicamento add constraint pk_medicamento primary key (Cod_M)
+ALTER TABLE Medicamento
+ADD CONSTRAINT pk_medicamento PRIMARY KEY (Cod_M)
 
-alter table Medicamento ADD constraint fk_consulta_medicamento foreign key (No_Consulta)
-References Consulta (No_Consulta)
+ALTER TABLE Medicamento
+ADD CONSTRAINT fk_consulta_medicamento FOREIGN KEY (No_Consulta) REFERENCES Consulta (No_Consulta)
 
-create table Alergico(
-	No_Control int not null,
-	Cod_M varchar(8)not null
+CREATE TABLE Alergia(
+	No_Control INT NOT NULL,
+	Cod_M VARCHAR(8) NOT NULL
 )
 
-alter table Alergico add constraint pk_alergico primary key (No_Control, Cod_M)
+ALTER TABLE Alergia
+ADD CONSTRAINT pk_alergia PRIMARY KEY (No_Control, Cod_M)
 
-alter table Alergico add constraint fk_alumno_alergico foreign key (No_Control)
-References Alumno (No_Control)
+ALTER TABLE Alergia
+ADD CONSTRAINT fk_alumno_alergia FOREIGN KEY (No_Control) REFERENCES Alumno (No_Control)
 
-alter table Alergico add constraint fk_medicamento_alergico foreign key (Cod_M)
-References Medicamento (Cod_M)
+ALTER TABLE Alergia
+ADD CONSTRAINT fk_medicamento_alergia FOREIGN KEY (Cod_M) REFERENCES Medicamento (Cod_M)
 
 
 

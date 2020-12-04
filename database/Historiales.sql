9b32a183793bbb0dd52728012d655008d2e755d9
RebecaValadez <47796126+RebecaValadez@users.noreply.github.com>
2020-12-04 14:19:17 -0600

    Add files via upload

---
+++ database/Historiales.sql
@@ -0,0 +1,66 @@
+create database Historiales
+
+use Historiales
+
+create table Alumno(
+	No_Control int not null,
+	Nombre varchar(50),
+	Sexo varchar(15),
+	Carrera varchar(30)
+)
+
+alter table Alumno add constraint pk_alumno primary key (No_Control)
+
+create table Médico(
+	Cédula int not null,
+	Nombre varchar(50),
+	Campus int
+)
+
+alter table Médico add constraint pk_medico primary key (Cédula)
+
+create table Consulta(
+	No_Consulta int not null,
+	No_Control int not null,
+	Cédula int not null,
+	Fecha_consulta date,
+	Diagnóstico varchar(30),
+	Tipo_Afeccion varchar(30)
+)
+
+alter table Consulta add constraint pk_consulta primary key (No_Consulta)
+
+alter table Consulta ADD constraint fk_alumno_consulta foreign key (No_Control) 
+References Alumno (No_Control)
+
+alter table Consulta ADD constraint fk_medico_consulta foreign key (Cédula) 
+References Médico (Cédula)
+
+create table Medicamento(
+	No_Consulta int not null,
+	Cod_M varchar(8)not null,
+	Nombre varchar(40)
+)
+
+alter table Medicamento add constraint pk_medicamento primary key (Cod_M)
+
+alter table Medicamento ADD constraint fk_consulta_medicamento foreign key (No_Consulta) 
+References Consulta (No_Consulta)
+
+create table Alérgico(
+	No_Control int not null,
+	Cod_M varchar(8)not null
+)
+
+alter table Alérgico add constraint pk_alergico primary key (No_Control, Cod_M)
+
+alter table Alérgico add constraint fk_alumno_alergico foreign key (No_Control) 
+References Alumno (No_Control)
+
+alter table Alérgico add constraint fk_medicamento_alergico foreign key (Cod_M) 
+References Medicamento (Cod_M)
+
+
+
+
+

6650aa6a1c2013cc5f5a70693b92eac5420a2047
Pablo <45538982+Pabloitl@users.noreply.github.com>
2020-12-08 13:19:09 -0600

    Secciones difinidas

--- database/Historiales.sql
+++ database/Historiales.sql
@@ -1,12 +1,16 @@
+-- DROP TABLE Consulta;
+-- DROP TABLE Alergia;
+-- DROP TABLE Medicamento;
+-- DROP TABLE Alumno;
+-- DROP TABLE Medico;
+--- Hasta este punto se va a quitar
+
+-- Script LDD
 CREATE DATABASE Historiales
 
 USE Historiales
 
-DROP TABLE Consulta;
-DROP TABLE Alergia;
-DROP TABLE Medicamento;
-DROP TABLE Alumno;
-DROP TABLE Medico;
+--- Creación de tablas con claves foraneas y primarias
 
 CREATE TABLE Alumno(
 	No_Control INT NOT NULL,
@@ -15,17 +19,11 @@ CREATE TABLE Alumno(
 	Carrera VARCHAR(30)
 );
 
-ALTER TABLE Alumno
-ADD CONSTRAINT pk_alumno PRIMARY KEY (No_Control);
-
 CREATE TABLE Medico(
 	Cedula INT NOT NULL,
 	Nombre VARCHAR(50),
 	Campus INT
 );
-
-ALTER TABLE Medico
-ADD CONSTRAINT pk_medico PRIMARY KEY (Cedula);
 
 CREATE TABLE Consulta(
 	No_Consulta INT NOT NULL,
@@ -37,49 +35,68 @@ CREATE TABLE Consulta(
     Cod_M VARCHAR(8) NOT NULL
 );
 
-ALTER TABLE Consulta
-ADD CONSTRAINT pk_consulta PRIMARY KEY (No_Consulta);
-
-ALTER TABLE Consulta
-ADD CONSTRAINT fk_alumno_consulta FOREIGN KEY (No_Control) REFERENCES Alumno (No_Control);
-
-ALTER TABLE Consulta
-ADD CONSTRAINT fk_medico_consulta FOREIGN KEY (Cedula) REFERENCES Medico (Cedula);
-
-ALTER TABLE Consulta
-ADD CONSTRAINT fk_medicamento_consulta FOREIGN KEY (Cod_M) REFERENCES Medicamento (Cod_M);
-
 CREATE TABLE Medicamento(
 	Cod_M VARCHAR(8) NOT NULL,
 	Nombre VARCHAR(40)
 );
-
-ALTER TABLE Medicamento
-ADD CONSTRAINT pk_medicamento PRIMARY KEY (Cod_M);
 
 CREATE TABLE Alergia(
 	No_Control INT NOT NULL,
 	Cod_M VARCHAR(8) NOT NULL
 );
 
+ALTER TABLE Alumno
+ADD CONSTRAINT pk_alumno PRIMARY KEY (No_Control);
+
+ALTER TABLE Medico
+ADD CONSTRAINT pk_medico PRIMARY KEY (Cedula);
+
+ALTER TABLE Consulta
+ADD CONSTRAINT pk_consulta PRIMARY KEY (No_Consulta);
+
+ALTER TABLE Consulta
+ADD CONSTRAINT fk_alumno_consulta FOREIGN KEY (No_Control)
+REFERENCES Alumno (No_Control);
+
+ALTER TABLE Consulta
+ADD CONSTRAINT fk_medico_consulta FOREIGN KEY (Cedula)
+REFERENCES Medico (Cedula);
+
+ALTER TABLE Consulta
+ADD CONSTRAINT fk_medicamento_consulta FOREIGN KEY (Cod_M)
+REFERENCES Medicamento (Cod_M);
+
+ALTER TABLE Medicamento
+ADD CONSTRAINT pk_medicamento PRIMARY KEY (Cod_M);
+
 ALTER TABLE Alergia
 ADD CONSTRAINT pk_alergia PRIMARY KEY (No_Control, Cod_M);
 
 ALTER TABLE Alergia
-ADD CONSTRAINT fk_alumno_alergia FOREIGN KEY (No_Control) REFERENCES Alumno (No_Control);
+ADD CONSTRAINT fk_alumno_alergia FOREIGN KEY (No_Control)
+REFERENCES Alumno (No_Control);
 
 ALTER TABLE Alergia
-ADD CONSTRAINT fk_medicamento_alergia FOREIGN KEY (Cod_M) REFERENCES Medicamento (Cod_M);
+ADD CONSTRAINT fk_medicamento_alergia FOREIGN KEY (Cod_M)
+REFERENCES Medicamento (Cod_M);
+
+-- Insertar registros en la BD
 
 
 -- Vistas
 
 -- Cuántas veces se han usado los medicamentos
-
 CREATE VIEW UsoMedicamentos_view
 AS
 SELECT Nombre, COUNT(*) AS NVecesUsado
 FROM Consulta JOIN Medicamento ON Consulta.Cod_M = Medicamento.Cod_M
 GROUP BY Nombre
 
+-- Creacion de usuarios y delimitacion de privilegios
 
+-- Creación de procedimientos almacenados para inserción de registros
+
+-- creación de procedimientos almacenados para consulta o
+-- actualizacion de la base de datos
+
+-- Definición de un trigger para controlar integridad de datos
